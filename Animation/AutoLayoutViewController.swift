b9e0f7fb38046b65787c262cfbb2c1b8f2292283
@candygu2011 <candygu2011@gmail.com>
2015-10-12 15:07:57 +0800

    Size Class & AutoLayout

--- Animation/AutoLayoutViewController.swift
+++ Animation/AutoLayoutViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AutoLayoutViewController: UINavigationController {
+class AutoLayoutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
