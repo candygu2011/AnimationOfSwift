370f0b8a7cb1aee575772e5ec4059d69276f752d
@candygu2011 <candygu2011@gmail.com>
2015-10-14 16:03:21 +0800

    Update UI &  AVAudioPlayer

--- Animation/TapMe.swift
+++ Animation/TapMe.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TapMe: UIViewController {
     
@@ -17,18 +18,27 @@ class TapMe: UIViewController {
     var seconds = 0
     var timer = NSTimer()
     
+    // sound
+    var buttonBeep:AVAudioPlayer?
+    var secondBeep:AVAudioPlayer?
+    var backgroundMusic:AVAudioPlayer?
+    
+    
     @IBAction func buttonPressed(sender: AnyObject) {
         println("Button Pressed")
         count++
         scoreLabel.text = "Score: \(count)"
+        buttonBeep?.play()
     }
     
     func setupGame() {
-        seconds = 10
+        seconds = 30
         count = 0
         timeLabel.text = "Timer:\(seconds)"
         scoreLabel.text = "Score:\(count)"
         timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "subtractTime", userInfo: nil, repeats: true)
+        backgroundMusic?.volume = 0.3
+        backgroundMusic?.play()
         
     }
     func subtractTime() {
@@ -36,6 +46,7 @@ class TapMe: UIViewController {
         count++
         timeLabel.text = "Time:\(seconds)"
         scoreLabel.text = "Score:\(count)"
+        secondBeep?.play()
 
         if (seconds == 0) {
             timer.invalidate()
@@ -48,7 +59,32 @@ class TapMe: UIViewController {
         }
     }
     
+    
+    // sound
+    func setupAudioPlayWithFile(file:String,type:String) -> AVAudioPlayer? {
+        // 1
+        let path = NSBundle.mainBundle().pathForResource(file, ofType: type)
+        let url  = NSURL.fileURLWithPath(path!)
+        
+        // 2
+        var audioPlayer:AVAudioPlayer?
+        
+        // 3
+        audioPlayer = AVAudioPlayer(contentsOfURL: url, error: nil)
+        return audioPlayer
+    }
+    // life cycle
     override func viewDidLoad() {
         setupGame()
+        view.backgroundColor = UIColor(red: 60/255.0, green: 60/255.0, blue: 60/255.0, alpha: 1)
+        if let buttonBeep = self.setupAudioPlayWithFile("ButtonTap", type: "wav") {
+            self.buttonBeep = buttonBeep
+        }
+        if let secondBeep = self.setupAudioPlayWithFile("SecondBeep", type:"wav") {
+            self.secondBeep = secondBeep
+        }
+        if let backgroundMusic = self.setupAudioPlayWithFile("HallOfTheMountainKing", type:"mp3") {
+            self.backgroundMusic = backgroundMusic
+        }
     }
 }
